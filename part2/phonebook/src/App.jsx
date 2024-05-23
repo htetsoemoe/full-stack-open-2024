202a437a8d0a2ed8c7925f03ce6924b2313e572c
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-05-23 17:50:47 +0630

    finished new user to server functionality

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,60 @@
 import { useEffect, useState } from 'react'
-import noteServices from './services/notes'
+import personService from './services/persons'
+
+/** Components */
+// Heading
+const Heading = ({ text }) => (
+  <h1>{text}</h1>
+)
+
+// Part component which used in PersonForm component
+const Part = ({ text, value, handleNewChange }) => {
+  return (
+    <div>
+      {text} <input value={value} onChange={handleNewChange} />
+    </div>
+  )
+}
+
+// Button
+const Button = ({ type, text, handleNewChange }) => {
+  return (
+    <button type={type} onClick={handleNewChange}>{text}</button>
+  )
+}
+
+// PersonForm 
+const PersonForm = ({ onSubmit, newName, newNumber, handleNewName, handleNewNumber }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <Part text={'name'} value={newName} handleNewChange={handleNewName} />
+      <Part text={'number'} value={newNumber} handleNewChange={handleNewNumber} />
+      <Button text='add' type='submit' />
+    </form>
+  )
+}
+
+// Notification
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [changeMessage, setChangeMessage] = useState('')
 
   useEffect(() => {
-    noteServices
+    personService
       .getAll()
       .then(persons => {
         setPersons(persons)
@@ -24,43 +71,57 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  const handleAdd = (event) => {
+  const handleAddPerson = (event) => {
     event.preventDefault()
+
     const newPerson = {
+      id: persons.length + 1,
       name: newName,
       number: newNumber,
-      id: persons.length + 1,
     }
 
+    // check new person name is already existed in persons
     const checkNewName = persons.find(person => person.name.toLowerCase() === newPerson.name.toLowerCase())
 
     if (checkNewName && checkNewName.number === newPerson.number) {
       window.alert(`${newName} is already added to phone book`)
     } else {
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setNewNumber('')
+      // Save new person to server
+      personService
+        .create(newPerson)
+        .then(savedPerson => {
+          setPersons(persons.concat(savedPerson))
+          setNewName('')
+          setNewNumber('')
+          setChangeMessage(`Successfully added Username: ${newName}`)
+          setTimeout(() => { // After 3 seconds later set null value to changeMessage.
+            setChangeMessage(null)
+          }, 3000)
+        })
+        .catch(error => {
+          setChangeMessage(`Error: ${error.response.data.error}`)
+        })
     }
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={handleAdd}>
-        <h2>Filter user</h2>
-        filter shown with: <input />
-        <h2>Add a new</h2>
-        <div>
-          name: <input onChange={handleNameOnChange} />
-        </div>
-        <div>
-          number: <input onChange={handleNumberOnChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
+      <Heading text={'Phonebook'} />
+      {/* Notification Component */}
+      <Notification message={changeMessage} />
+      {/* Filter Component */}
+
+      <Heading text={'add a new'} />
+      {/* PersonForm Component */}
+      <PersonForm
+        onSubmit={handleAddPerson}
+        newName={newName}
+        newNumber={newNumber}
+        handleNewName={handleNameOnChange}
+        handleNewNumber={handleNumberOnChange}
+      />
+
+      <Heading text={'Numbers'} />
       {persons.map((person) => (
         <p key={person.id}>{person.name} {person.number}</p>
       ))}
