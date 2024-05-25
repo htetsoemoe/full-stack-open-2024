ae60c8788390f3099711a790905ff8c604e207f8
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-05-25 12:22:31 +0630

    dataforcountries project data fetching

---
+++ part2/dataforcountries/src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [country, setCountry] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => setCountries(response.data))
+  }, [])
+
+  console.log(countries)
+
+  const handleCountryChange = (event) => {
+    setCountry(event.target.value)
+  }
+
+  const cssStyles = {
+    display: 'flex',
+    gap: '25px',
+    alignItems: 'center',
+    padding: '10px',
+    margin: '20px',
+    fontWeight: 'bold',
+    fontSize: '20px'
+  }
+
+  const imgStyles = {
+    width: '70px'
+  }
+
+  return (
+    <div>
+      <form>
+        Find Countries <input value={country} onChange={handleCountryChange} />
+      </form>
+      <div>
+        {countries.map((country, index) => {
+          return (
+            <div
+              key={index}
+              style={cssStyles}
+            >
+              <div>
+                {`Country: ${country.altSpellings}, `}
+              </div>
+              <div>
+                {`City: ${country.capital}, `}
+              </div>
+              <div>
+                {`Continent: ${country.continents}, `}
+              </div>
+              <img style={imgStyles} src={country.flags.png} />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
