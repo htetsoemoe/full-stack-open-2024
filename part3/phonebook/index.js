e6ee68ef5bbce76a8a9f8ff63f965f7c48596495
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-05-30 15:11:09 +0630

    part3 phonebooke project exercises 3.1-3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,10 +6,92 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const PORT = process.env.PORT || 3500
+const PORT = process.env.PORT || 3001
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
 
 app.get('/', (req, res) => {
     res.status(200).json({msg: "Hello World form Phone Book"})
+})
+
+app.get('/api/persons', (req, res) => {
+    res.status(200).json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date()
+    const count = persons.length
+    res.send(`
+        <p>Phonebook has info for ${count} people</p> 
+        <p>${date}</p>
+    `)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(person => person.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if(persons.find(person => person.name === body.name)) {
+        return res.status(400).json({error: "name must be unique."})
+    } else if (!body.name) {
+        return res.status(400).json({error: "name is missing."})
+    } else if (!body.number) {
+        return res.status(400).json({error: "number is missing."})
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person) //Combines two or more arrays. This method returns a new array without modifying any existing arrays
+    res.status(201).json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
 })
 
 app.listen(PORT, () => {
