1f154cac23613b37c7ffd2cc22dbc8869c84b8cd
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-06-12 18:37:28 +0630

    config and use separate variables for the database addresses of the development and test databases

--- part4/blog/utils/config.js
+++ part4/blog/utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3001
-const MONGODB_URL = process.env.MONGODB_URL
+const MONGODB_URL = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URL
 
 module.exports = {
     PORT,
